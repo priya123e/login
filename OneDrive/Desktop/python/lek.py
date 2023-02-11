ffc78316d00bece64120fd19084bc919aecfde11
srilekha <srilekhatokala@gmail.com>
2023-02-10 23:50:25 -0500

    first commit

---
+++ OneDrive/Desktop/python/lek.py
@@ -0,0 +1,10 @@
+def employee_hours(work_hours):
+    current_max = 0
+    employee_of_month =''
+    for employee,hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+    return (employee_of_month,current_max)

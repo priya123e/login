a7a2a4e48017682f9b25e6b5f21a3c41b729d397
priya123e <111388531+priya123e@users.noreply.github.com>
2023-02-11 00:23:19 -0500

    added print statment

--- OneDrive/Desktop/python/sri.py
+++ OneDrive/Desktop/python/sri.py
@@ -5,5 +5,6 @@ def check_even_list(num_list):
             even_number.append(number) 
         else:
             pass
+    print("CHANGES IN GIT")
     return even_number 
  check_even_list([4,5,6,78])
